b21f4033fe129aa5a6d49f428418848bb6c6a5f6
Eric Andresen <eric@andresen.codes>
2018-02-01 19:17:29 +0100

    Placed welcome on index, Created register user modal.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,28 @@
 $(document).ready( function() {
     $("#loginButton").on("click", function() {
-        $("#mainContent").load('welcome.html');
+        $.get("/user/", function (data) {
+            alert(data);
+        })
     });
-});
 
-$("#registerButton").on("click", function () {
-    $("#mainContent").load('registerUser.html')
+    $("#registerButton").on("click", function () {
+        $('#myModal').modal('show')
+    });
+
+    $("#createAccountButton").on("click", function () {
+        var url = "http://localhost:8080/user/";
+
+        var userName = $('#formControlInputName').val();
+        var userPassword = $('#formControlInputPassword').val();
+
+        var user = JSON.stringify({ "username": userName, "password": userPassword });
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: user,
+            dataType: 'json',
+            contentTyp: 'application/json'
+        });
+    });
 });
